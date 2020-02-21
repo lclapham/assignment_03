c435685bb026f92bed827b84abfb46d50abee332
Lamar Clapham <lamar.clapham@gmail.com>
2020-02-20 18:35:13 -0800

    problems_1-3_completed

---
+++ js/01-functions-practice.js
@@ -0,0 +1,30 @@
+//STEP 1
+
+function halfNumber(num1) {
+    return console.log ("Half of " + num1 + " is " + (num1/2));
+}
+
+halfNumber(10);
+
+//STEP 2
+
+function squareNumber(num1) {
+    return console.log ("The result of squaring the " + num1 + " is " + Math.pow(num1, 2));
+}
+
+squareNumber(12);
+
+//STEP 3
+
+function percentOf(num1, num2){
+    return console.log (num1 + " is "+ (num1/num2)*100 + "%" + " of " + num2);
+}
+
+percentOf(30, 100);
+
+//STEP 4
+
+//STEP 5
+
+
+
