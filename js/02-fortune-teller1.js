515044b0fcd3cffe90370b0391f4d4cea698badb
Lamar Clapham <lamar.clapham@gmail.com>
2020-02-27 13:23:15 -0800

    Made a redirect of index.

---
+++ js/02-fortune-teller1.js
@@ -0,0 +1,77 @@
+let partnersName;
+let jobTitle;
+let jobLocation;
+let numChildern;
+
+// Set up handers to make interaction with page
+var handlers = {
+    tellFortune: function () {
+        launchGame();
+        display.displayFortue();
+
+    }
+}
+
+// Setup  to display it
+// NOT IN KEEPING WITH THE ASSIGNEMENT CHECK THE CONSOLE LOG TO SEE IT PRINT 3X'S
+var display = {
+    displayFortue: function () {
+        let fortunePara = document.querySelector('p');
+        fortunePara.innerHTML = fortuneResults;
+       
+    }
+};
+
+// This is the game launcher.  Might be better to put in click handler
+function launchGame() {
+    let numOfTimes = 3;
+    for (i = 0; i < 3; i++) {
+        nameGenerator();
+        jobGenerator();
+        locationGenerator();
+        childGenerator();
+        tellFortune(partnersName, jobTitle, jobLocation, numChildern);
+    }
+
+}
+
+// This fucntion compiles the results of the fortune
+function tellFortune(partnersName, jobTitle, jobLocation, numChildern) {
+    console.log("You will be a " + jobTitle + " in " + jobLocation + ", and married to " + partnersName + " with " + numChildern + " kids." );
+    return fortuneResults = "You will be a " + jobTitle + " in " + jobLocation + ", and married to " + partnersName + " with " + numChildern + " kids." + "<br>";
+}
+
+function nameGenerator() {
+    // word lists
+    let firstName = ["Tim", "Rebecca", "Nicole", "Donald", "Ronald", "Nancy", "Anthony", "Allison"];
+    let lastName = ["Smith", "Clinton", "Wienstein", "Shumer", "Shiff", "Pelosi", "Cortez", "Munson"];
+    // generate name
+    partnersName = firstName[Math.floor(Math.random() * firstName.length)] + " " + lastName[Math.floor(Math.random() * lastName.length)];
+    return partnersName;
+}
+
+function jobGenerator() {
+    // word lists
+    let jobs = ["street Sweeper", "CEO", "web developer", "maid", "bartender", "stripper", "Uber driver", "astronaut", "disk jockey"];
+    // generate name
+    jobTitle = jobs[Math.floor(Math.random() * jobs.length)];
+    return jobTitle;
+}
+
+function locationGenerator() {
+    // word lists
+    let location = ["USA", "Canada", "India", "Thailand", "Australia", "Phillipines", "Russia", "China", "Germany"];
+    // generate name
+    jobLocation = location[Math.floor(Math.random() * location.length)];
+    return jobLocation;
+}
+
+function childGenerator() {
+    numChildern = (Math.floor(Math.random() * 10))
+    if (numChildern === 0) {
+        numChildern = "no"  // future update would be to dynamically change kids to kid when no or 1 kids is picked.
+    } else {
+        return numChildern;
+    }
+
+}
