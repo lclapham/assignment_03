f9e5c72f27500a827ef3df503ccee95b107c9826
Lamar Clapham <lamar.clapham@gmail.com>
2020-02-27 11:26:14 -0800

    removed the extra returns in function

--- js/03-rock-paper-scissors.js
+++ js/03-rock-paper-scissors.js
@@ -16,45 +16,43 @@ do {
 
 
     if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
-        window.alert("You must enter \"rock, paper or scissors\" ")
+        window.alert("You must enter \"rock, paper or scissors\" ");
         gameRun = true
 
     } else if (userChoice === computerChoice) {
-        window.alert("It was a tie, try again")
+        window.alert("It was a tie, try again");
         gameRun = true;
 
     } else {
-       validate(userChoice, computerChoice);
-       runAgain = window.prompt("Play again y/n?")
-       if (runAgain !== "y" && runAgain !== "n"){
-           window.alert("You must enter \"y or n\"")
-           gameRun = false;
-       } else if (runAgain === 'y'){
-           gameRun =true;
-       } else {
-           gameRun = false;
-       }
+        validate(userChoice, computerChoice);
+
+        runAgain = window.prompt("Play again y/n?");
+        if (runAgain !== "y" && runAgain !== "n") {
+            window.alert("You must enter \"y or n\"")
+            gameRun = false;
+        } else if (runAgain === 'y') {
+            gameRun = true;
+        } else {
+            gameRun = false;
+        }
     }
 
- } while (gameRun)
+} while (gameRun)
 
 
-function validate (userChoice, computerChoice) {
+function validate(userChoice, computerChoice) {
     if (userChoice === "rock" && computerChoice === "scissors") {
         window.alert("Player wins with rock");
-         return gameRun = false;
 
     } else if (userChoice === "scissors" && computerChoice === "paper") {
         window.alert("Player wins with scissors");
-        return gameRun = false;
 
     } else if (userChoice === "paper" && computerChoice === "rock") {
         window.alert("Player wins with paper");
-        return gameRun = false;
 
     } else {
         window.alert("Computer wins with " + computerChoice);
-        return gameRun = false;
+
     }
 
 }
